b4e42c3d29118c1e659a2b20f51bdb7fb27819d0
nawa aripurna <nap.aripurna@gmail.com>
2019-05-27 21:35:03 +0800

    initial commit

---
+++ lib/quill_parser.rb
@@ -0,0 +1,6 @@
+require "quill_parser/version"
+
+module QuillParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/quill_parser/version.rb
@@ -0,0 +1,3 @@
+module QuillParser
+  VERSION = "0.1.0"
+end
